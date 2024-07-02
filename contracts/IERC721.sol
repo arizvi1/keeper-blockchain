5ab4047c56934bce637c3dee87e0737f4992ec06
Abdullah <abdullah.khalid@codingcops.com>
2024-07-03 02:44:30 +0500

    Revert "m"
    This reverts commit aad52eb17f8f72befc6764fbfeddba2d5dd7ca6f.

--- contracts/IERC721.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-// ============ Interfaces ============
-interface IERC721  {
-    function ownerOf(uint256 tokenId) external view returns (address owner);
-}
